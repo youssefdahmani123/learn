7ba1053645176d6a8b2d7de26cfc0fa069b4e783
Firas Dahmani <firas1dahmani@gmail.com>
2024-08-03 20:13:56 +0200

    i learn variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+character_name="firas"
+age ="84"
+print("there was a boy called "+character_name+ " dahmani")
+print("he was " +age+ " years old")
+print("he liked his "+character_name)
+print("but wasn't happy about being "+age)
+print(character_name+" has one brother")
