c1981a60191917f5929f3f38b6e3cf23d30389b8
Firas Dahmani <firas1dahmani@gmail.com>
2024-08-06 15:52:32 +0200

    Create mad libs.py
    i learn mad libs

---
+++ mad libs.py
@@ -0,0 +1,8 @@
+color = input("enter a color:")
+plural_noun = input("entre a plural noun: ")
+adjective,= input("enter an object:")
+
+
+print ("trees are "+color")
+print (plural_nous+" are mean" )
+print ("pleas keep it "+ adjective)
